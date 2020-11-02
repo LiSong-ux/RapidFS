c729f66fc5ae2309c30978b6f2792234ad9e5eaf
LiSong-ux <546993317@qq.com>
2020-11-02 22:57:21 +0800

    新增shell启动脚本

---
+++ assembly/bin/rapidfs_tracker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# shellcheck disable=SC2164
+cd ../lib
+command=$1;
+# shellcheck disable=SC2077
+# shellcheck disable=SC2140
+if [ "$command"=="start" ]; then
+java -jar -Djava.ext.dirs=./:"$JAVA_HOME"/jre/lib/ext tracker-0.1.0.jar
+else echo "Command Error"
+fi
